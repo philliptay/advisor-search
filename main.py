a8670cc1fa20abe31f56ef3de6d067fa671b688d
kolbym <47865185+kolbym@users.noreply.github.com>
2019-11-10 07:54:51 -0500

    Add files via upload
    initalized the sql query code, still not sure how to code the rest with the html format and such but it should be simple enough once we bring it all together

--- main.py
+++ main.py
@@ -11,6 +11,29 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def index():
 
+    area = request.args.get('area')
+    if (area is None) or (area.strip() == ''):
+        area = ''
+    else:
+        dept = dept.strip().lower()
+
+    DATABASE_NAME = 'reg.sqlite'
+    if not path.isfile(DATABASE_NAME):
+        raise Exception('Database connection failed')
+    connection = connect(DATABASE_NAME)
+    cursor = connection.cursor()
+    cursor.execute('SELECT profs.name, profs.contact, areas.area, profs.bio FROM areas, profs WHERE areas.profid = profs.profid AND area = \"' + area + '\"')
+    rows = cursor.fetchall()
+
+    # for row in rows:
+    # add prof name to table in html code
+    # pass cookies for each profs contact and bio
+    # not sure how it will work for profs with multiple areas
+
+
+
+
+
     # database = Database()
     # database.connect()
     # results = database.search(formInfo)
