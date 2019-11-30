d02009f176ca056d852f93f7ec75581099901fee
Aaron Lichtblau <aaronlichtblau@Aarons-MacBook-Pro.local>
2019-11-29 23:14:28 -0500

    taking out login/sign up from database.py

--- database.py
+++ database.py
@@ -115,55 +115,6 @@ class Database:
         professor = Professor(name, bio, areas, projects, titles, links, contact)
         return professor
 
-    def loginSearch(self, username, password):
-        #check that username is in Database
-        #if not found, print error message: username does not exist
-        #check that password matches username
-        #if not print incorrect password
-        cursor = connection.cursor()
-        stmtStr = 'SELECT username, password FROM login WHERE username = ?'
-        cursor.execute(stmtStr, [username])
-
-        row = cursor.fetchone()
-        if row is None:
-            cursor.close()
-            connection.close()
-            return "username does not exist error"
-        else:
-            if password != row[1]:
-                cursor.close()
-                connection.close()
-                return "incorrect password"
-            else:
-                cursor.close()
-                connection.close()
-                return "success"
-
-    def addLogin(self, username, password):
-        userpair = '(' + username + ', ' + password + ')'
-        cursor = connection.cursor()
-        stmtStr = 'SELECT username FROM login WHERE username = ?'
-        cursor.execute(stmtStr, [username])
-        row = cursor.fetchone()
-        #check that username is not already there
-        if row is not None:
-            cursor.close()
-            connection.close()
-            # if so send message that user exists, pick a different name
-            return "username already exists, please select a different username"
-        else:
-            cursor.close()
-            connection.close()
-
-            #add username to username column and password to password column
-            cursor = connection.cursor()
-            stmtStr2 = 'INSERT INTO login (username, password) VALUES ?'
-            cursor.execute(stmtStr2, [userpair])
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return 'successful creation of new user'
-
     def rankResults(self, results):
         profDict = {}
         # loop through results list
