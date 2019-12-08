8782771542632ef08b524f14f388c23fac96eff4
Van Brantley <van.brantley@gmail.com>
2019-12-08 17:30:35 -0500

    input filtering code (commented)

--- main.py
+++ main.py
@@ -49,6 +49,15 @@ def searchResults():
     areas = request.args.getlist('areas')
     keywords = request.args.getlist('keywords')
 
+    # areas = []
+    # keywords = []
+    # tags = request.args.getlist('tags')
+    # for tag in tags:
+    #     if tag in allAreas:
+    #         areas.append(tag)
+    #     else:
+    #         keywords.append(tag)
+
     searchInput = [areas, keywords]
 
     database = Database()
