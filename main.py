ba582e9c569ec1470831d3702460fdea3f5f7fdf
Van Brantley <van.brantley@gmail.com>
2020-01-05 18:33:15 -0500

    resizing complete

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ def searchResults():
             topAreas += prof[1][i]+', '
         topAreas = topAreas.rstrip(', ')
 
-        if database.isProfFavorited(username, prof[2]):
-            active = 'active'
+        # if database.isProfFavorited(username, prof[2]):
+        #     active = 'active'
 
         html += '<div class=prof'+str(prof[2])+' onclick="getProfResults('+str(prof[2])+');"><li class="list-group-item" tabindex="0"><div class="flex-container-row"><div class="flex-item-stretch truncate"><strong>'+str(prof[0])+'</strong></div><div class="flex-item-rigid"><i data-toggle="tooltip" data-original-title="Click to favorite" class="fa fa-heart fav-icon '+active+'" onclick="getFavorited('+str(prof[2])+');"></i></div></div><br><span>Top Areas: '+ topAreas +'</span></li></div>'
     html += '</ul></div>'
